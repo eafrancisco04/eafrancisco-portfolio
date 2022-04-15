71ad1e8652e55fd412a7ef029c185aaf8fb045a9
Ea Francisco <emberkasai04@gmail.com>
2022-04-15 10:13:32 -0400

    add banenr

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import { SiAdobephotoshop } from "react-icons/si";
+
 function Home() {
   return (
-    <>
-    <h1>Testing</h1>
-    </>
+    <main>
+      <div className="banner">
+        <h1>Ea Francisco</h1>
+        <h3>Freelancer Web Designer and Content Manager</h3>
+        <button>View My Portfolio</button>
+      </div>
+      <div className="skills">
+        <h2>My Skills</h2>
+        <div className="skills-icons">
+          <SiAdobePhotoshop />
+        </div>
+      </div>
+
+    </main>
   );
 }
 
