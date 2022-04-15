6086248eb0aad781f5f52b123e1d5bb1cfeac224
Ea Francisco <emberkasai04@gmail.com>
2022-04-15 10:33:55 -0400

    fix icons area

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import { SiAdobephotoshop } from "react-icons/si";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiAdobeaftereffects } from "react-icons/si";
+import { SiAdobexd } from "react-icons/si";
 
 function Home() {
   return (
@@ -11,7 +14,10 @@ function Home() {
       <div className="skills">
         <h2>My Skills</h2>
         <div className="skills-icons">
-          <SiAdobePhotoshop />
+          <SiAdobephotoshop size={70}/>
+          <SiAdobeillustrator size={70} />
+          <SiAdobeaftereffects size={70} />
+          <SiAdobexd size={70} />
         </div>
       </div>
 
